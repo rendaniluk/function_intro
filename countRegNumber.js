ea02e936372920acd8fe287943f25fe89f350748
rendaniluk <lukhwa11582924@gmail.com>
2016-11-04 22:25:45 +0200

    initial commit

--- countRegNumber.js
+++ countRegNumber.js
@@ -1,8 +1,12 @@
+const assert = require('assert')//importing assertto js
+//declaring function
 var countRegNumber = function(regNum){
-   var str = regNum.split(",");
-   var strD = str.length;
+   str = regNum.split(",");
+   str = str.length;
   return str;
-  console.log(strD);
-};
 
-var regcount= countRegNumber('CA 182736,CY 523519,CJ 812328')
+};
+//calling function with the countRegNumber string
+var regcount= countRegNumber('CA 182736,CY 523519,CJ 812328,AA 11 BR GP')
+console.log(regcount); //printing the length of string
+assert.equal(regcount, 4) //testing code using assert function
