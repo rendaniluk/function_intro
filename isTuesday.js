0e7b779a0e94f7a3903f1115cf01b962637a6c10
rendaniluk <lukhwa11582924@gmail.com>
2016-11-05 19:54:08 +0200

    initial commit

--- isTuesday.js
+++ isTuesday.js
@@ -5,11 +5,17 @@ Today = day.startsWith('Tu');
 return Today;
 };
 //storing function result in variable
-var theDay=isTuesday('Tuesday');
-var thDay=isTuesday('Monday');
+var theDay=isTuesday('Monday');
+var thDay=isTuesday('Tuesday');
+var thiDay=isTuesday('Wednesday');
+var thaDay=isTuesday('Thursday');
+var thoDay=isTuesday('Friday');
 //printing result of the function
 console.log(theDay);
 console.log(thDay);
+console.log(thiDay);
+console.log(thaDay);
+console.log(thoDay);
 //verifying the expected result using assert function
 assert.equal(isTuesday('Tuesday'),true);
 assert.equal(isTuesday('Monday'),false);
