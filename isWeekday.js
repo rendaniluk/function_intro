7468ad243413a35ee4de50c2f03ee3f354ae1627
rendaniluk <lukhwa11582924@gmail.com>
2016-11-07 16:24:10 +0200

    initial commit

--- isWeekday.js
+++ isWeekday.js
@@ -1,1 +1,13 @@
 const assert = require('assert'); //importing assert function to JS.
+//declaring function isWeekday
+var isWeekday = function(days){
+    var weekdays = !days.startsWith('S');
+  return weekdays;
+};
+
+var mon=isWeekday('Monday');
+console.log(mon);
+var Sun=isWeekday('Sunday');
+console.log(Sun);
+assert.equal(isWeekday('Monday'),true)
+assert.equal(isWeekday('Sunday'),false)
